d36c69314ba24ad4b00fd9b094c0722d32d5416e
cscrawford <carmensleight@gmail.com>
2023-10-26 22:41:46 -0400

    everything seems to work but coolant

--- pool_table_game.py
+++ pool_table_game.py
@@ -10,6 +10,16 @@ shield = pg.Surface((1000,600))
 reflector = pg.Surface((950 ,550))
 gameboard = pg.Surface((900, 500))
 start_button = pg.Surface((75,50))
+game_over_message = pg.Surface ((600,300))
+poison_meter = pg.Surface ((75,50))
+poison_down = pg.Surface ((25,25))
+poison_up = pg.Surface ((25,25))
+coolant_flow_meter_label = pg.Surface((100,25))
+poison_meter_label = pg.Surface((100,25))
+coolant_flow_meter = pg.Surface((75,50))
+coolant_flow_down = pg.Surface ((25,25))
+coolant_flow_up = pg.Surface ((25,25))
+
 clock = pg.time.Clock()
 
 # Booleans
@@ -17,10 +27,14 @@ running = True
 fuel_to_place = False
 moderator_to_place = False
 poison_to_place = False
+coolant_to_place = False
 fuel_cursor_growing = False
 moderator_cursor_growing = False
 poison_cursor_growing = False
 coolant_cursor_growing = False
+rxn_started = False
+supercritical = False
+
 
 # colors
 poison_color = "brown"
@@ -28,8 +42,13 @@ coolant_color = "blue"
 
 # numbers
 dt = 0
+t = 0
 fission_count = 0
 cursor_size = 0
+coolant_flow_rate = 0
+poison_effectiveness = 0.1
+criticality = 0
+max_fissions = 0
 
 # lists
 neutrons = []
@@ -37,6 +56,7 @@ fuel_spots = []
 moderator_spots = []
 poison_spots = []
 coolant_spots = []
+neutron_count = []
 
 # cursor
 starting_cursor = pg.cursors.Cursor(*pg.cursors.arrow)
@@ -49,6 +69,20 @@ shield_pos = pg.Vector2((screen.get_width()-shield.get_width())/2,(screen.get_he
 reflector_pos = pg.Vector2((screen.get_width()-reflector.get_width())/2, (screen.get_height()-reflector.get_height()) / 2)
 gameboard_pos = pg.Vector2((screen.get_width()-gameboard.get_width()) / 2, (screen.get_height()-gameboard.get_height()) / 2)
 start_button_pos = pg.Vector2(screen.get_width()-25-start_button.get_width(), (screen.get_height()-25-start_button.get_height()))
+
+# coolant meter
+coolant_flow_meter_pos = pg.Vector2(start_button_pos.x, (start_button_pos.y-25-coolant_flow_meter.get_height()))
+coolant_flow_up_pos = pg.Vector2(coolant_flow_meter_pos.x-coolant_flow_up.get_width(),coolant_flow_meter_pos.y)
+coolant_flow_down_pos = pg.Vector2(coolant_flow_meter_pos.x-coolant_flow_down.get_width(),coolant_flow_meter_pos.y+coolant_flow_up.get_height())
+coolant_flow_meter_label_pos = pg.Vector2(coolant_flow_up_pos.x,coolant_flow_meter_pos.y-coolant_flow_meter_label.get_height())
+
+# poison meter
+poison_meter_pos = pg.Vector2(start_button_pos.x, (coolant_flow_meter_label_pos.y-25-poison_meter.get_height()))
+poison_up_pos = pg.Vector2(coolant_flow_meter_pos.x-poison_up.get_width(),poison_meter_pos.y)
+poison_down_pos = pg.Vector2(coolant_flow_meter_pos.x-poison_down.get_width(),poison_meter_pos.y+poison_up.get_height())
+poison_meter_label_pos = pg.Vector2(poison_up_pos.x,poison_meter_pos.y-poison_meter_label.get_height())
+
+game_over_message_pos =  pg.Vector2((screen.get_width()-game_over_message.get_width()) / 2, (screen.get_height()-game_over_message.get_height()) / 2)
 
 
 neutron_size = 10
@@ -124,7 +158,7 @@ def grow_cursor(name,spot_color):
     surf = pg.Surface((cursor_size,cursor_size)) # you could also load an image 
     surf.fill("white")        # and use that as your surface
     pg.draw.circle(surf,spot_color, (int(cursor_size/2),int(cursor_size/2)), cursor_size/2)
-    font = pg.font.Font(None, 30)
+    font = pg.font.Font(None, 12)
     text = font.render(name, True, "white")
     textpos = text.get_rect(centerx=int(cursor_size/2), y=int(cursor_size/2))
     surf.blit(text, textpos) 
@@ -144,6 +178,9 @@ def place_spot(spot_type):
 def touching_circle(position1,position2,size1,size2=0):
     return ((position1.x - position2.x) **2 + (position1.y - position2.y)**2)**0.5 < size1 + size2
 
+def touching_rectrangle(surface1,position1,position2):
+    return (position1.x < position2.x and position1.x + surface1.get_width() > position2.x and position1.y < position2.y and position1.y + surface1.get_height() > position2.y)
+
 def add_text(string,pos,surface,color="white",fontsize = 30):
     font = pg.font.Font(None, fontsize)
     text = font.render(string, True, color)
@@ -158,8 +195,8 @@ while running:
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
-            click_pos =  pg.Vector2((event.pos[0],event.pos[1]))
-            if started = False:
+            click_pos =  pg.Vector2(event.pos[0],event.pos[1])
+            if rxn_started == False:
                 if touching_circle(click_pos,fuel_button_pos,fuel_button_size):
                     cursor_size = 0
                     fuel_cursor_growing = True
@@ -172,9 +209,19 @@ while running:
                 if touching_circle(click_pos,coolant_button_pos,coolant_button_size):
                     cursor_size = 0
                     coolant_cursor_growing = True
-            if touching_circle(click_pos,start_button_pos,100):
-                started = True
-                birth()
+            if touching_rectrangle(start_button,start_button_pos,click_pos) and rxn_started == False:
+                rxn_started = True
+                for i in range(50):
+                    birth()
+            if touching_rectrangle(poison_up,poison_up_pos,click_pos):
+                poison_effectiveness += 0.01
+            if touching_rectrangle(poison_down,poison_down_pos,click_pos):
+                poison_effectiveness -= 0.01
+            if touching_rectrangle(coolant_flow_up,coolant_flow_up_pos,click_pos):
+                coolant_flow_rate += 0.01
+            if touching_rectrangle(coolant_flow_down,coolant_flow_down_pos,click_pos):
+                coolant_flow_rate -= 0.01
+            
         elif event.type == pg.MOUSEBUTTONUP:
             if fuel_cursor_growing == True:
                 fuel_cursor_growing = False
@@ -217,11 +264,29 @@ while running:
     shield.fill("gray")
     reflector.fill("green")
     start_button.fill("orange")
-    add_text("fissions:" + str(fission_count),pg.Vector2(screen.get_width() / 2, 10),screen,'black')
+    poison_meter.fill("white")
+    coolant_flow_meter.fill("white")
+    poison_meter_label.fill("orange")
+    coolant_flow_meter_label.fill("orange")
+    poison_up.fill("brown")
+    coolant_flow_up.fill("brown")
+    poison_down.fill("brown")
+    coolant_flow_down.fill("brown")
+    add_text("criticality:" + str(0.01 * int(100*criticality)),pg.Vector2(screen.get_width() / 2, 10),screen,'black')
+    add_text("operation time:" + str(int(t)) + " seconds",pg.Vector2(screen.get_width() -200, 10),screen,'black')
+
     add_text("neutrons:" + str(len(neutrons)),pg.Vector2(screen.get_width() / 2, 40),screen,'black')
     add_text("shielding",pg.Vector2(shield.get_width() / 2, 10),shield,'black')
     add_text("reflector",pg.Vector2(reflector.get_width() / 2, 10),reflector,'black')
     add_text("start",pg.Vector2(start_button.get_width() / 2, start_button.get_height()/2),start_button,'black')
+    add_text(str(int(poison_effectiveness*100))+"%",pg.Vector2(poison_meter.get_width() / 2, poison_meter.get_height()/2),poison_meter,'black')
+    add_text(str(int(coolant_flow_rate*100))+"%",pg.Vector2(coolant_flow_meter.get_width() / 2, coolant_flow_meter.get_height()/2),coolant_flow_meter,'black')
+    add_text("^",pg.Vector2(poison_up.get_width() / 2, poison_up.get_height()/2),poison_up,'black')
+    add_text("^",pg.Vector2(coolant_flow_up.get_width() / 2, coolant_flow_up.get_height()/2),coolant_flow_up,'black')
+    add_text("v",pg.Vector2(poison_down.get_width() / 2, poison_down.get_height()/2),poison_down,'black')
+    add_text("v",pg.Vector2(coolant_flow_down.get_width() / 2, coolant_flow_down.get_height()/2),coolant_flow_down,'black')
+    add_text("poison insertion",pg.Vector2(poison_meter_label.get_width() / 2, poison_meter_label.get_height()/2),poison_meter_label,'black',fontsize = 15)
+    add_text("coolant flow rate",pg.Vector2(coolant_flow_meter_label.get_width() / 2, coolant_flow_meter_label.get_height()/2),coolant_flow_meter_label,'black',fontsize = 15)
 
 
 
@@ -240,7 +305,7 @@ while running:
     add_text("moderator",moderator_button_pos,screen)
     
     for spot in poison_spots:
-        poison_spot = pg.draw.circle(gameboard, poison_color, spot['position'], spot['size'])
+        poison_spot = pg.draw.circle(gameboard, poison_color, spot['position'], spot['size']*poison_effectiveness)
         add_text("p",spot['position'],gameboard,fontsize=12)
 
     
@@ -256,6 +321,7 @@ while running:
     add_text("coolant",coolant_button_pos,screen)
             
     i = 0
+    fissions_this_step = 0
     for neutron in neutrons:    
         pg.draw.circle(gameboard, "red", neutron['position'],neutron_size)
         if neutron['position'].y >  gameboard.get_height():
@@ -270,20 +336,30 @@ while running:
             if touching_circle(neutron['position'],fuel['position'],neutron_size,fuel['size']):
                 if (neutron['velocity'].x**2 + neutron['velocity'].y**2)**0.5 < birth_speed/2:
                     fission(i)
+                    fissions_this_step += 1
         for moderator in moderator_spots:
             if touching_circle(neutron['position'],moderator['position'],neutron_size,moderator['size']):
                 neutron = moderated_bounce(moderator,neutron,i)
         for poison in poison_spots:
-            if touching_circle(neutron['position'],poison['position'],neutron_size,poison['size']):
+            if touching_circle(neutron['position'],poison['position'],neutron_size,poison['size']*poison_effectiveness) and poison_effectiveness > 0:
                 death(i)
         i += 1        
         neutron['position'].x += neutron['velocity'].x * dt
         neutron['position'].y += neutron['velocity'].y * dt
 
+
     screen.blit(shield,shield_pos) 
     screen.blit(reflector,reflector_pos) 
     screen.blit(gameboard,gameboard_pos) 
     screen.blit(start_button,start_button_pos) 
+    screen.blit(poison_meter,poison_meter_pos) 
+    screen.blit(coolant_flow_meter,coolant_flow_meter_pos) 
+    screen.blit(poison_meter_label,poison_meter_label_pos) 
+    screen.blit(coolant_flow_meter_label,coolant_flow_meter_label_pos) 
+    screen.blit(poison_up,poison_up_pos) 
+    screen.blit(coolant_flow_up,coolant_flow_up_pos) 
+    screen.blit(poison_down,poison_down_pos) 
+    screen.blit(coolant_flow_down,coolant_flow_down_pos) 
 
 
     # flip() the display to put your work on screen
@@ -293,5 +369,71 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
+    if rxn_started:
+        t += dt
+    neutron_count.append(len(neutrons))
+    if len(neutron_count) > 75:
+        neutron_count.pop(0)
+    if len(neutron_count) > 2:
+        if neutron_count[0] > 50:
+            criticality = neutron_count[-1]/neutron_count[0]
+            if criticality > 1.2:
+                supercritical = True
+
+
+    max_fissions = max(max_fissions,fissions_this_step)
+
+    while supercritical == True:
+        game_over_message.fill("yellow")
+        add_text("Oh no! Your reactor has reached uncontrollable supercricitacality.", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-75),game_over_message,"black",25)
+        add_text("Total Operation Time: " + str (int (t)) + " seconds.", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-50),game_over_message,"black",25)
+        add_text("Maximum Power Level: " + str(int(max_fissions*200)) + " MeV/second", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-25),game_over_message,"black",25)
+        add_text("Game Over", pg.Vector2(game_over_message.get_width()/2, game_over_message.get_height()/2),game_over_message,"black",50)    
+        add_text("Click anywhere to restart.", pg.Vector2( game_over_message.get_width()/2,game_over_message.get_height()/2+50),game_over_message,"black",25)
+        screen.blit(game_over_message,game_over_message_pos) 
+        pg.display.flip()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                fission_count = 0
+                criticality = 0
+                poison_effectiveness = 0.1
+                neutrons = []
+                fuel_spots = []
+                moderator_spots = []
+                poison_spots = []
+                coolant_spots = []
+                neutron_count = []
+                supercritical = False
+                rxn_started = False
+
+                
+    while rxn_started == True and len(neutrons)==0:
+        game_over_message.fill("yellow")
+        add_text("Oh no! Your reactor has run out of neutrons.", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-75),game_over_message,"black",25)
+        add_text("Total Operation Time: " + str(int(t)) + " seconds.", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-50),game_over_message,"black",25)
+        add_text("Maximum Power Level: " + str(int(max_fissions*200)) + " MeV/second", pg.Vector2(game_over_message.get_width()/2,game_over_message.get_height()/2-25),game_over_message,"black",25)
+        add_text("Game Over", pg.Vector2(game_over_message.get_width()/2, game_over_message.get_height()/2),game_over_message,"black",50)    
+        add_text("Click anywhere to restart.", pg.Vector2( game_over_message.get_width()/2,game_over_message.get_height()/2+50),game_over_message,"black",25)
+        screen.blit(game_over_message,game_over_message_pos) 
+        pg.display.flip()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                fission_count = 0
+                t = 0
+                criticality = 0
+                poison_effectiveness = 0.1
+                neutrons = []
+                fuel_spots = []
+                moderator_spots = []
+                poison_spots = []
+                coolant_spots = []
+                neutron_count = []
+                supercritical = False
+                rxn_started = False
 
 pg.quit()
+
